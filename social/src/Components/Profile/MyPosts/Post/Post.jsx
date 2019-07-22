ac71b94d88d798222e7b1e654d0122631099bc0f
harut666 <aspt273@gmail.com>
2019-07-22 21:46:54 +0400

    улучшил структуру папок
    разделил на более мелкие компоненты  MyPosts Post

---
+++ social/src/Components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './Post.module.css';
+
+
+const Post = () => {
+    return (
+    <div className={s.item}>
+                    <img src='https://clck.ru/HDS84'/>
+                    post 1
+                    <div>
+                    <span>like</span>
+                    </div>
+           </div>
+    )
+}
+
+export default Post;
--- social/src/Components/Profile/Profile.jsx
+++ social/src/Components/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import s from './Profile.module.css';
-
+import MyPosts from './MyPosts/MyPosts'
 
 const Profile = () => {
     return <div className={s.content}>
@@ -10,18 +10,7 @@ const Profile = () => {
         <div>
             avatar + description
         </div>
-        <div>
-            my posts
-            <div> new posts</div>
-            <div className={s.posts}>
-                <div className={s.item}>
-                    post 1
-                </div>
-                <div className={s.item}>
-                    post 2
-                </div>
-            </div>
-        </div>
+        <MyPosts/>
     </div>
 
 }
