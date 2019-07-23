1af14ad809bc4cc0c25c261f084ed892f4c71531
harut666 <aspt273@gmail.com>
2019-07-23 12:10:17 +0400

    вывод через Props постов

--- social/src/Components/Profile/MyPosts/MyPosts.jsx
+++ social/src/Components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
+
 const MyPosts = () => {
     return <div>
             My posts
@@ -10,9 +11,11 @@ const MyPosts = () => {
                 <button>Add post</button>
             </div>
             <div className={s.posts}>
-                <Post />
+                <Post message='Hi, how are you?'/>
+                <Post message='It`s my first post'/>
             </div>
         </div>
+
 
 }
 
--- social/src/Components/Profile/MyPosts/Post/Post.jsx
+++ social/src/Components/Profile/MyPosts/Post/Post.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import s from './Post.module.css';
 
 
-const Post = () => {
+const Post = (props) => {
     return (
     <div className={s.item}>
                     <img src='https://clck.ru/HDS84'/>
-                    post 1
-                    <div>
-                    <span>like</span>
+                    {props.message}
                     </div>
-           </div>
     )
+
 }
+
 
 export default Post;
