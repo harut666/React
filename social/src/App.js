40d78ba9c013ba87b8e312ecc4db5e3f298cebbd
harut666 <aspt273@gmail.com>
2019-07-23 23:40:09 +0400

    20 добавил модуль Route
    У нас ведь теперь есть 2 конмпоненты главные (Dialogs и Profile), по сути, 2 разные страницы! И нам хотелось бы между ними как-то переключаться!!!
    
    И тут нам на помощь приходит компонента Route
    
    Как она работает? Компонент Route просто следит за адресной строкой браузера и ... Если замечает, что там отображается "свой" (нужный ей) URL, то автоматом отрисовывает нужную компоненту!
    
    А вот то, что в данном уроке я прописывал:
    
    * установка пакета:
    npm i react-router-dom -save

--- social/src/App.js
+++ social/src/App.js
@@ -5,19 +5,21 @@ import Header from './Components/Header/Header'
 import Navbar from './Components/Navbar/Navbar'
 import Profile from './Components/Profile/Profile'
 import Dialogs from "./Components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 const App = (props) => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <div className={'app-wrapper-content'}>
-            <Dialogs/>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className={'app-wrapper-content'}>
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                </div>
             </div>
-        {/*<Profile/>*/}
-
-        </div>
-    )
+        </BrowserRouter>
+)
 }
 
 export default App;
--- social/src/Components/Navbar/Navbar.jsx
+++ social/src/Components/Navbar/Navbar.jsx
@@ -4,12 +4,22 @@ import s from './Navbar.module.css';
 
 const Navbar = () => {
     return <nav className={s.nav}>
-        <div className={s.item}><a>Profile</a></div>
-        <div className={s.item}><a>Messages</a></div>
-        <div className={s.item}><a>News</a></div>
-        <div className={s.item}><a>Music</a></div>
+        <div className={s.item}>
+            <a href='/profile'>Profile</a>
+        </div>
+        <div className={s.item}>
+            <a href='/dialogs'>Messages</a>
+        </div>
+        <div className={s.item}>
+            <a>News</a>
+        </div>
+        <div className={s.item}>
+            <a>Music</a>
+        </div>
         <br/>
-        <div className={s.item}><a>Setting</a></div>
+        <div className={s.item}>
+            <a>Setting</a>
+        </div>
     </nav>
 
 }
