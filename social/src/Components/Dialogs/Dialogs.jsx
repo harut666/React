a0400c43eaced289bef9b3427d77cad30da22b42
harut666 <aspt273@gmail.com>
2019-07-24 10:44:56 +0400

    22 Cверстал страницу Диалогов

--- social/src/Components/Dialogs/Dialogs.jsx
+++ social/src/Components/Dialogs/Dialogs.jsx
@@ -3,7 +3,31 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) =>{
     return(
-        <div>Dialogs</div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog}>
+                    Kamo
+                </div>
+                <div className={s.dialog}>
+                    Arut
+                </div>
+                <div className={s.dialog}>
+                    Karine
+                </div>
+                <div className={s.dialog}>
+                    Hovo
+                </div>
+                <div className={s.dialog}>
+                    Arman
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>what are you doing </div>
+                <div className={s.message}>come here</div>
+            </div>
+        </div>
+
     )
 }
 
