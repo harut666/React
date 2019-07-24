38aede2db2f923c55201efad8e5bf25f2626d885
harut666 <aspt273@gmail.com>
2019-07-24 10:07:59 +0400

    Убрал перезагрузку страныцы при переходе NavLink
    Убрал перезагрузку страныцы при переходе NavLink и добавил подсветку активной страныцы activeClassName . еще убрал подчеркивания у ссылок
    .item a{
        text-decoration: none;
    }

--- social/src/Components/Navbar/Navbar.jsx
+++ social/src/Components/Navbar/Navbar.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import s from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 
 const Navbar = () => {
-    return <nav className={s.nav}>
+    return (
+    <nav className={s.nav}>
         <div className={s.item}>
-            <a href='/profile'>Profile</a>
+            <NavLink to='/profile' activeClassName={s.activeLink}>Profile</NavLink>
         </div>
         <div className={s.item}>
-            <a href='/dialogs'>Messages</a>
+            <NavLink to='/dialogs' activeClassName={s.activeLink}>Messages</NavLink>
         </div>
         <div className={s.item}>
-            <a>News</a>
+            <NavLink>News</NavLink>
         </div>
         <div className={s.item}>
-            <a>Music</a>
+            <NavLink>Music</NavLink>
         </div>
         <br/>
         <div className={s.item}>
-            <a>Setting</a>
+            <NavLink>Setting</NavLink>
         </div>
     </nav>
-
+    )
 }
 
 export default Navbar;
